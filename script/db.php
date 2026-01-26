fb581fecef1e74cf061a895520ad87ba3dcd8c1a
jacopinopio <j.ricciardelli@studenti.unisa.it>
2026-01-26 12:44:38 +0100

    wwww

--- script/db.php
+++ script/db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "torronificio");
+$servername = "localhost";
+$username = "root";
+$password = "";  // lascia vuoto se non hai password per root
+$dbname = "torronificio nardone"; // nome del database con spazio
 
+// Crea connessione
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Controlla connessione
 if ($conn->connect_error) {
-    die("Errore di connessione");
+    die("Connessione fallita: " . $conn->connect_error);
 }
 ?>
--- script/login.php
+++ script/login.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
-include "db.php";
+include "db.php"; // Assumendo che login.php sia in "script/"
 
-$email = $_POST["email"];
+// Prendi i dati dal form
+$email = $_POST["username"];  // il form manda "username" anche se in realtà è l'email
 $password = $_POST["password"];
 
-$sql = "SELECT * FROM login WHERE email='$email'";
+// Controlla che l'utente esista
+$sql = "SELECT * FROM `login` WHERE `email`='$email'";
 $result = $conn->query($sql);
 
 if ($result->num_rows === 1) {
@@ -13,7 +15,9 @@ if ($result->num_rows === 1) {
 
     if (password_verify($password, $user["password"])) {
         $_SESSION["login"] = $user["email"];
-        header("Location: Home.html");
+        // Redirect corretto alla home, salendo di una cartella e entrando in codice html
+        header("Location: ../codice html/Home.html");
+        exit();
     } else {
         echo "Password errata";
     }
