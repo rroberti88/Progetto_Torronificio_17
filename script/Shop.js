933c6678ddb3ab20982e2ade43efe3351dcbf65c
Feliand21 <f.iandoli@studenti.unisa.it>
2026-01-27 11:08:21 +0100

    aggiornamenti

--- script/Shop.js
+++ script/Shop.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
         noResults.style.display = visibleCount === 0 ? "block" : "none";
 
         // 4) Aggiorna titolo sopra
-        const nomeCat = (currentCategory === "all") ? "Tutti i prodotti" : getNomeCategoriaAttiva();
+        const nomeCat = (currentCategory === "all") ? "Tutti i prodotti" :"";
         if (searchTerm.length > 0) {
             titoloCategoria.textContent = `${nomeCat} — risultati per: "${searchBar.value.trim()}"`;
         } else {
@@ -100,35 +100,4 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       console.error("Link carrello non trovato!");
     }
-  
-    const userIcon = document.getElementById("login");
-    if (!userIcon) {
-      console.error("Icona/login link non trovato!");
-      return;
-    }
-  
-    userIcon.addEventListener("click", function (e) {
-      e.preventDefault();
-  
-      Swal.fire({
-        title: "Hai già un account?",
-        imageUrl: "../immagini jpg/LogoNardone.jpg",
-        imageWidth: 120,
-        imageHeight: 80,
-        imageAlt: "Logo Torronificio Nardone",
-        showCancelButton: true,
-        confirmButtonText: "Sì, intendo loggarmi",
-        cancelButtonText: "No, procedo con la registrazione",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.href = "Login.html?mode=login";
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-          // premuto "No, procedo con la registrazione"
-          window.location.href = "Login.html?mode=register";
-        } else {
-          // click fuori / ESC / X
-          window.location.href = "Shop.html";
-        }
-      });
-    });
-  });
+});
