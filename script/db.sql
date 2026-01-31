d604b35f807d8c47cc96de58df4e82eae07bad7e
Feliand21 <f.iandoli@studenti.unisa.it>
2026-01-31 16:57:22 +0100

    commit database

---
+++ script/db.sql
@@ -0,0 +1,34 @@
+
+DROP TABLE IF EXISTS users cascade;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    data_nascita DATE
+);
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO www;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO www;
+INSERT INTO users  (nome, cognome, email, data_nascita, username, password) VALUES ( --esempio di utente
+    'Robi', 
+    'Ribi', 
+    'Robi@esempio.com', 
+    '1990-01-01', 
+    'CdkLover', 
+    '$2y$10$8K1p/vG9.N1p/vG9.N1p/vuo3YvG9.N1p/vG9.N1p/vG9.N1p/' -- Questo è un hash d'esempio per 'www'
+);
+
+
+-- UPDATE users
+-- SET email = 'nuova@email.com'
+-- WHERE username = 'CdkLover';
+
+-- UPDATE users
+-- SET password = 'HASH_NUOVO'
+-- WHERE id = 1;
+
+-- UPDATE users
+-- SET nome = 'Mario', cognome = 'Rossi'
+-- WHERE id = 1;
